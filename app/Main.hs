9ef598e153c5228ce289fb931fcaf1aaef3e4a2e
Tapani Toivanen <TTOI@oticonmedical.com>
2018-01-11 15:12:55 +0100

    First commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Hasky
+
+main :: IO ()
+main = print (myDrop 2 [1..10])
---
+++ src/Hasky.hs
@@ -0,0 +1,7 @@
+module Hasky ( myDrop ) where
+
+    myDrop :: Int -> [a] -> [a]
+    myDrop n xs = 
+        if n <= 0  || null xs
+            then xs
+            else myDrop (n - 1) (tail xs)

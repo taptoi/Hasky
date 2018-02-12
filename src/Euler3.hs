b2c1126676008b7d69c1730e2ea863676743098e
Tapani Toivanen <TTOI@oticonmedical.com>
2018-02-12 12:50:01 +0100

    Updated euler3

--- src/Euler3.hs
+++ src/Euler3.hs
@@ -1,21 +1,6 @@
-module Euler3 ( euler3 ) where
-    
-    primes = 2 : filter (isPrime) [3, 5 ..]
-    isPrime n = isPrime' n primes
-                where 
-                isPrime' n (p:ps)
-                    | p * p > n      = True
-                    | n `rem` p == 0 = False
-                    | otherwise = isPrime' n ps
-
-
-    maxPrimeFactor n = 
-        maxPrimeFactor' n primes 2
-        where
-        maxPrimeFactor' c (p:ps) acc
-            | p * p > n      = acc
-            | c `rem` p /= 0 = maxPrimeFactor' c ps acc -- not a factor
-            | c `rem` p == 0 = maxPrimeFactor' (c `quot` p) ps p -- prime factor found
+module Euler3 (euler3) where 
+    import Data.List
+    import Hasky(maxPrimeFactor)
 
     euler3 :: Integer
     euler3 = maxPrimeFactor 600851475143

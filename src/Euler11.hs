03c778308e50b427c89ea7ff64248b1fc8fae63c
taptoi <toivasentapani@gmail.com>
2018-02-24 02:34:39 +0100

    add euler11

--- src/Euler11.hs
+++ src/Euler11.hs
@@ -1,4 +1,21 @@
 module Euler11 ( euler11 ) where
-    import Hasky (primesTo)
+    import Data.List(transpose)
+    import Hasky(listsOf, ints)
+    
+    shiftRows :: [[Integer]] -> [[Integer]]
+    shiftRows [] = []
+    shiftRows [r] = [r]
+    shiftRows (row:rows) = row : shiftRows' rows [0] 
+                        where
+                            shiftRows' [] zs = [] 
+                            shiftRows' [r] zs = [zs ++ r]
+                            shiftRows' (r:rs) zs = (zs ++ r) : shiftRows' rs (0 : zs)
+
+    rotateCw = map reverse . transpose
+    diagonals m = map (filter (\x -> x /= 0)) $ transpose $ shiftRows m
+    allDiagonals m = (diagonals m) ++ (diagonals $ transpose m)
+    allSeqsOf n m = filter (not . null) ( (map (listsOf n) m) ++ map (listsOf n) (allDiagonals m) )
+    solve m = maximum $ map (foldl1 (*)) (concat (allSeqsOf 4 m))
+
     euler11 :: String -> Integer
-    euler11 s =  
+    euler11 s =  solve ( map ints (lines s) )
--- src/Hasky.hs
+++ src/Hasky.hs
@@ -1,4 +1,5 @@
-module Hasky (primes, primesTo, maxPrimeFactor, isPrime) where
+module Hasky (primes, primesTo, maxPrimeFactor, isPrime, listsOf, digits, ints) where
+    import Data.Char(digitToInt, isHexDigit)
     primes = 2 : filter (isPrime) [3, 5 ..]
     isPrime n = isPrime' n primes
                 where 
@@ -16,3 +17,12 @@ module Hasky (primes, primesTo, maxPrimeFactor, isPrime) where
             | p * p > n      = acc
             | c `rem` p /= 0 = maxPrimeFactor' c ps acc -- not a factor
             | c `rem` p == 0 = maxPrimeFactor' (c `quot` p) ps p -- prime factor found
+
+    listsOf n xs = filter (\a -> length a == n) $ listsOf' xs 
+            where 
+                listsOf' [] = []
+                listsOf' [d] = []
+                listsOf' (d:ds) = take n ([d] ++ ds) : listsOf' ds
+
+    digits s = map (toInteger . digitToInt) $ filter isHexDigit s
+    ints s = map (\x -> read x :: Integer) (words s)

458a76fac07028e46bfab5580d3dfed596af74a4
taptoi <toivasentapani@gmail.com>
2018-02-25 10:05:30 +0100

    update euler11

--- src/Euler11.hs
+++ src/Euler11.hs
@@ -12,10 +12,14 @@ module Euler11 ( euler11 ) where
                             shiftRows' (r:rs) zs = (zs ++ r) : shiftRows' rs (0 : zs)
 
     rotateCw = map reverse . transpose
-    diagonals m = map (filter (\x -> x /= 0)) $ transpose $ shiftRows m
+    diagonals = map (filter (\x -> x /= 0)) . transpose . shiftRows
     allDiagonals m = (diagonals m) ++ (diagonals $ transpose m)
-    allSeqsOf n m = filter (not . null) ( (map (listsOf n) m) ++ map (listsOf n) (allDiagonals m) )
-    solve m = maximum $ map (foldl1 (*)) (concat (allSeqsOf 4 m))
+    allDiagsOf n = map (listsOf n) . allDiagonals
+    adjacentXsOf n = map (listsOf n)
+    adjacentYsOf n = (adjacentXsOf n) . transpose
+    allAdjacentXsAndYsOf n m = (adjacentXsOf n m) ++ (adjacentYsOf n m)
+    allAdjacentsOf n m = (allAdjacentXsAndYsOf n m) ++ (allDiagsOf n m)
+    solve = maximum . map product . concat . allAdjacentsOf 4
 
     euler11 :: String -> Integer
-    euler11 s =  solve ( map ints (lines s) )
+    euler11 =  solve . map ints . lines

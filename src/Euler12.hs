eb477fa847a0f96b0b616af71fdaf65d4140076a
Tapani Toivanen <TTOI@oticonmedical.com>
2018-02-28 13:34:24 +0100

    Add euler 12 and refactor some things

--- src/Euler12.hs
+++ src/Euler12.hs
@@ -1,6 +1,7 @@
 module Euler12 ( euler12 ) where
-    import Hasky (primeFactors)
+    import Hasky (numFactors)
     triangles = scanl1 (+) [ 1, 2 .. ]
+    triangleFactors = map (\t -> (t, numFactors t)) triangles
 
     euler12 :: Integer
-    euler12 = 1
+    euler12 = fst . head . filter (\n -> snd n > 500) $ triangleFactors
--- src/Hasky.hs
+++ src/Hasky.hs
@@ -1,6 +1,9 @@
-module Hasky (primes, primesTo, maxPrimeFactor, primeFactors, isPrime, listsOf, digits, ints) where
+module Hasky (primes, primesTo, maxPrimeFactor, primeFactors, isPrime, listsOf, digits, ints, numFactors) where
     import Data.Char(digitToInt, isHexDigit)
     import Data.List(tails)
+    import Data.Maybe(fromJust)
+    import Data.List(nub)
+    
     primes = 2 : filter (isPrime) [3, 5 ..]
     isPrime n = isPrime' n primes
                 where 
@@ -12,16 +15,37 @@ module Hasky (primes, primesTo, maxPrimeFactor, primeFactors, isPrime, listsOf, 
     primesTo n = takeWhile (<= n) primes
 
     primeFactors n = 
-        primeFactors' n primes []
+        nub . reverse $ (primeFactors' n primes [])
         where
         primeFactors' c (p:ps) acc
-            | p > c          = acc
-            | c `rem` p /= 0 = primeFactors' c ps acc -- not a factor
-            | c `rem` p == 0 = primeFactors' (c `quot` p) ps (p:acc) -- prime factor found
+            | p * p > n && null acc = [p]
+            | p > c                 = acc
+            | c `rem` p /= 0        = primeFactors' c ps acc -- not a factor
+            | c `rem` p == 0        = primeFactors' (c `quot` p) (p:ps) (p:acc) -- prime factor found
 
     maxPrimeFactor = maximum . primeFactors
 
-    -- factors n = concat . map (\pf -> [pf * 1, pf * 2 .. (n-1)]) . primeFactors $ n
+    -- if number prod has a prime factor of base, e.g. base^a = prod
+    -- return the maximum exponent a of base that prod is divisible with
+    primeFactorExponent prod base = primeFactorExponent' prod 0 
+                                    where 
+                                        primeFactorExponent' prod' acc 
+                                            | prod' <= 0                         = Nothing
+                                            | acc == 0 && prod' `rem` base == 0  = primeFactorExponent' (prod' `quot` base) 1
+                                            | prod' `rem` base == 0              = primeFactorExponent' (prod' `quot` base) (acc + 1)
+                                            | prod' `rem` base /= 0 || prod' == 1 = Just acc
+                                            | otherwise                          = Nothing
+
+
+    -- prime factors and their powers that divide n evenly
+    -- for n = p_1^a · p_2^b ··· p_k^r, returns
+    -- [(p_1, a), (p_2, b), .. (p_k, r)]
+    primeFactorPowers :: Integer -> [(Integer, Integer)]
+    primeFactorPowers n =  map (\pf -> (pf, fromJust ( primeFactorExponent n pf ))) . primeFactors $ n
+  
+    -- using the Product Rule of counting (http://en.wikipedia.org/wiki/Rule_of_product), we know that there will be
+    -- m = (a + 1)·(b + 1)···(r + 1)
+    numFactors = product . map (\pfp -> snd pfp + 1) . primeFactorPowers
 
     pairs :: [a] -> [[a]]
     pairs xs = [ [x,y] | (x:rest) <- tails xs , y <- rest ]
